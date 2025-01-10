3d860826e8e66d34eff97b303844612d66868d81
idanbabayov <idanbabayov@gmail.com>
2025-01-10 22:11:30 +0200

    accurate linear control loop

--- spawn_and_reach/turtle_navigator.py
+++ spawn_and_reach/turtle_navigator.py
@@ -61,7 +61,7 @@ class TurtleNavigator(Node):
 
 
             msg = Twist()
-            msg.angular.z = 6.0*dtheta
+            #msg.angular.z = 6.0*dtheta
             msg.linear.x = 2*dx
             msg.linear.y = 2*dy
 
